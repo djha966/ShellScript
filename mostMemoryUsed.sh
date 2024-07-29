aed7eeb339e5de30a44b0618e09d7dc325a8d620
djha966 <46783664+djha966@users.noreply.github.com>
2024-07-29 00:46:40 -0400

    Update mostMemoryUsed.sh

--- mostMemoryUsed.sh
+++ mostMemoryUsed.sh
@@ -1,1 +1,16 @@
 df -h | grep -v File | sort -rnk 4 | grep -v 6.3G | head -1 | awk '{print "your file system   "  $1  "is utilization   " $4 "  space"}'
+
+
+
+
+
+awk 'BEGIN {print " Name   Salary"} {print $1 "  " $2} END{print "============================"}' salary
+awk 'BEGIN {print "Name   Salary"} {print $1 "  " $2} END{print "============================"}' salary
+awk 'BEGIN {print "Name   Salary\n ========================="} {print $1 "  " $2} END{print "============================"}' salary
+awk 'BEGIN {print "Name   Salary\n========================="} {print $1 "  " $2} END{print "============================"}' salary
+awk 'BEGIN {print "Name   Salary\n========================="} {print $1 "  " $2} END{print "============================"}' salary > sal2.sh
+ 
+awk 'BEGIN { total=0} {print "salary of " $1 " is "$2} END{}' sal
+awk 'BEGIN { total=0} {print "salary of " $1 " is "$2; total = total + $2 } END{}' sal
+awk 'BEGIN { total=0} {print "salary of " $1 " is "$2; total = total + $2 } END{print "you salary is required to all" total}' sal
+awk 'BEGIN { total=0} {print "salary of " $1 " is "$2; total = total + $2 } END{print "you salary is required to all   " total}' sal
